d266f96f5fff3a58cb3f3c4dc68e982fb2379d89
Kevin <kwang44@buffalo.edu>
2021-04-02 14:44:59 -0400

    Did reim dao

--- WilliamServlet/src/main/java/com/WangWick/dao/ReimbursementDao.java
+++ WilliamServlet/src/main/java/com/WangWick/dao/ReimbursementDao.java
@@ -1,26 +1,21 @@
 package com.WangWick.dao;
 
 import java.util.List;
+
+import com.WangWick.model.User;
 import com.WangWick.util.HibernateUtil;
-import org.apache.log4j.Logger;
 
 import com.WangWick.model.Reimbursement;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import org.hibernate.query.Query;
 
 /*
  * Purpose of this Dao is to send/retrieve info about a reimbursement
  * to/from the database. It then returns the composed Reimbursement Object.
  */
 public class ReimbursementDao implements com.WangWick.dao.GenericDao<Reimbursement> {
-    private SessionFactory sessionFactory;
-    private static final Logger LOGGER = Logger.getLogger(ReimbursementDao.class);
 
 
     @Override
@@ -32,68 +27,48 @@ public class ReimbursementDao implements com.WangWick.dao.GenericDao<Reimburseme
             transaction = session.beginTransaction();
             reimbursementList = session.createQuery("FROM reimbursements").list();
             transaction.commit();
-            LOGGER.debug("A list of reimbursement was retrieved from the database.");
         }
         catch (Exception e) {
             e.printStackTrace();
-            LOGGER.error("An attempt to get all reimbursements failed.");
         }
         return reimbursementList;
     }
 
     @Override
     public Reimbursement getById(int id) {
-        Reimbursement reimbursement;
-
-
-
-        Reimbursement r = null;
-        try {
-            Session session = sessionFactory.getCurrentSession();
-            r = session.get(Reimbursement.class, id);
-            LOGGER.debug("A reimbursement by ID " + id + " was retrieved from the database.");
-        } catch (HibernateException e) {
-            e.printStackTrace();
-            LOGGER.error("An attempt to get a reimbursement by ID" + id + " from the database failed.");
+        Reimbursement reimbursement = null;
+        Transaction transaction = null;
+        
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            reimbursement = session.get(Reimbursement.class, id);
+            transaction.commit();
         }
-        return r;
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return reimbursement;
     }
 
     @Override
     public List<Reimbursement> getByUserId(int id) {
-        List<Reimbursement> l = null;
-        try {
-            Session session = sessionFactory.getCurrentSession();
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-            CriteriaQuery<Reimbursement> criteria = builder.createQuery(Reimbursement.class);
-            Root<Reimbursement> root = criteria.from(Reimbursement.class);
-            criteria.select(root).where(builder.equal(root.get("reimb_author"), id));
+        List<Reimbursement> reimbursementList = null;
+        Transaction transaction = null;
 
-            l = session.createQuery(criteria).getResultList();
-            LOGGER.debug("A list of reimbursements made by user ID " + id + " was retrieved from the database.");
-            return l;
-        } catch (HibernateException e) {
-            e.printStackTrace();
-            LOGGER.error("An attempt to get all reimbursements made by user ID " + id + " fron the database failed.");
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            transaction = session.beginTransaction();
+            Query query = session.createQuery("FROM reimbursements R WHERE R.author = :id");
+            User user = new User();
+            user.setUser_id(id);
+            query.setParameter("id", user);
+            reimbursementList = query.list();
+            transaction.commit();
         }
-//		try(Connection c = ConnectionUtil.getInstance().getConnection()) {
-//			String qSql = "SELECT * FROM ers_reimbursement WHERE reimb_author = ?";
-//			PreparedStatement ps = c.prepareStatement(qSql);
-//			ps.setInt(1, id);
-//			ResultSet rs = ps.executeQuery();
-//
-//			while(rs.next()) {
-//				l.add(objectConstructor(rs));
-//			}
-//			rs.close();
-//			ps.closeOnCompletion();
-//			LOGGER.debug("A list of reimbursements made by user ID " + id + " was retrieved from the database.");
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			LOGGER.error("An attempt to get all reimbursements made by user ID " + id + " fron the database failed.");
-//		}
-//		System.out.println(l.toString());
-        return l;
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return reimbursementList;
     }
 
     public Reimbursement getByUsername(String username) {
@@ -105,29 +80,27 @@ public class ReimbursementDao implements com.WangWick.dao.GenericDao<Reimburseme
     public void insert(Reimbursement r) {
         Transaction transaction = null;
 
-        try(Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
             transaction = session.beginTransaction();
             session.save(r);
             transaction.commit();
-            LOGGER.debug("A new reimbursement was successfully added to the database.");
-        } catch (HibernateException e) {
-            e.printStackTrace();
-            LOGGER.error("An attempt to insert a reimbursement to the database failed.");
-            assert transaction != null;
-            transaction.rollback();
         }
-
+        catch (Exception e) {
+            e.printStackTrace();;
+        }
     }
 
     @Override
     public void delete(Reimbursement r) {
-        try {
-            sessionFactory.getCurrentSession().delete(r);
-            LOGGER.debug("A reimbursement was successfully added to the database.");
-        } catch (HibernateException e) {
-            e.printStackTrace();
-            LOGGER.error("An attempt to remove a reimbursement from the database failed.");
-        }
+        Transaction transaction = null;
 
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(r);
+            transaction.commit();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- WilliamServlet/src/test/java/com/WangWick/dao/ReimbursementDaoTest.java
+++ WilliamServlet/src/test/java/com/WangWick/dao/ReimbursementDaoTest.java
@@ -1,18 +1,58 @@
 package com.WangWick.dao;
 
 import com.WangWick.model.Reimbursement;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import com.WangWick.model.User;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ReimbursementDaoTest {
 
+    @Test
+    @Order(1)
+    void createReimbustmentTest(){
+        UserDao userDao = new UserDao();
+        User user = new User(50000);
+        userDao.insert(user);
+        user = userDao.getByUsername(user.getUsername());
+
+        ReimbursementDao reimbursementDao = new ReimbursementDao();
+        for (int i = 0; i < 50; i++) {
+            Reimbursement reimbursement = new Reimbursement(i);
+            reimbursement.setAuthor(user);
+            reimbursement.setResolver(user);
+            reimbursementDao.insert(reimbursement);
+        }
+    }
 
     @Test
-    void createReimbustmentTest(){
-        ReimbursementDao reimbursementDao = new ReimbursementDao();
-        reimbursementDao.insert(new Reimbursement());
+    @Order(2)
+    void getAllTest() {
+        UserDao userDao = new UserDao();
+        User user = new User(50000);
+        userDao.insert(user);
+        user = userDao.getByUsername(user.getUsername());
 
+        ReimbursementDao reimbursementDao = new ReimbursementDao();
+        List<Reimbursement> reimbursementList = reimbursementDao.getList();
+        List<Reimbursement> reimbursementList1 = reimbursementDao.getByUserId(user.getUser_id());
+
+        for (int i = 0; i < 50; i++) {
+            Reimbursement reimbursement = reimbursementList.get(i);
+            Reimbursement reimbursement1 = reimbursementList1.get(i);
+            Assertions.assertEquals(reimbursement.getId(), reimbursement1.getId());
+        }
+    }
+
+    @Test
+    @Order(6)
+    void deleteAllTest(){
+        ReimbursementDao reimbursementDao = new ReimbursementDao();
+        List<Reimbursement> reimbursementList = reimbursementDao.getList();
+
+        for (Reimbursement reimbursement : reimbursementList) {
+            reimbursementDao.delete(reimbursement);
+        }
     }
 }
