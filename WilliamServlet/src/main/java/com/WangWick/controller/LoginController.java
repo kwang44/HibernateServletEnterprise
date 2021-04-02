cdd31b6136b00dffb8fa6295997c9de3d21af63f
nwickh03 <79473056+nwickh03@users.noreply.github.com>
2021-04-01 23:59:18 -0400

    began Login servlet implementation

--- WilliamServlet/src/main/java/com/WangWick/controller/LoginController.java
+++ WilliamServlet/src/main/java/com/WangWick/controller/LoginController.java
@@ -34,16 +34,16 @@ public class LoginController extends FrontController {
     public void handle(HttpServletRequest req, HttpServletResponse res) {
 
         try {
-            //LoginAttempt attempt = gb.fromJson(req.getReader(), LoginAttempt.class);
-           // User u = us.getUserByLogin(attempt.getUsername(),attempt.getPassword());
-            if(true){
+            LoginAttempt attempt = gb.fromJson(req.getReader(), LoginAttempt.class);
+            User u = us.getUserByLogin(attempt.getUsername(),attempt.getPassword());
+            if(u!=null){
                 HttpSession session= req.getSession(true);
 /*                                          if we make sure to check that valid session already
                                             exists by using req.getSession(false) everywhere else we force the user
                                             to login after 15 minutes of inactivity and lets us store role info in session
                                             to restrict use of different functions.
 */
-                session.setAttribute("user",new User());
+                session.setAttribute("user",u);
                 res.setContentType("text/json");
                 res.getWriter().print("{\"id\":"+gb.toJson(session.getId())+"}");
             }
